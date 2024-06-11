9c763c97ce4438a3cd4a564407528d32f19c5aa8
ogbotemi-2000 <oogbotemi@gmail.com>
2024-06-11 06:39:13 -0700

    Specifying empty echo command as build script and overriding output directory as blank to prevent errors in Vercel deployment

--- noop.js
+++
@@ -1,5 +0,0 @@
-let fs = require('fs');
-/*Vercel requires that a public folder is created after the build script is run
-hence the faux operation below
-*/
-!fs.existsSync('public')&&fs.mkdirSync('public')
